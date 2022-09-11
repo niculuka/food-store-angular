e5591b043626df053c9b77223fcbc945fbe51c6f
niculuka <niculuka@yahoo.com>
2022-09-11 17:16:06 +0300

    updates2

---
+++ src/app/order-agreement/order-agreement.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order-agreement',
+  templateUrl: './order-agreement.component.html',
+  styleUrls: ['./order-agreement.component.css']
+})
+export class OrderAgreementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
